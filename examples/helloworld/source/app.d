c218d805b28765a3440be25c0929d096c6cfa01f
Geod24 <pro.mathias.lang@gmail.com>
2022-01-19 18:53:50 +0900

    Add parseConfigFileSimple and simplify onboarding
    The current example is way too verbose about what is being done.
    This new function should cover the vast majority of use cases,
    and provide users with a simple 3 lines setup.

--- examples/helloworld/source/app.d
+++ examples/helloworld/source/app.d
@@ -4,6 +4,7 @@ import agora.config.Config;
 
 import std.getopt;
 import std.stdio;
+import std.typecons;
 import core.thread;
 
 struct Config
@@ -23,42 +24,19 @@ struct Config
 
 int main (string[] args)
 {
-    CLIArgs clargs;
-    // This calls `getopt` under the hood, and parses `config|c` and `O|override`
-    // This step is *optional* but recommended.
-    auto helpInformation = clargs.parse(args);
-    if (helpInformation.helpWanted)
-    {
-        defaultGetoptPrinter("Some information about the program.",
-            helpInformation.options);
-        return 0; // Not an error, so exit normally
-    }
-
-    try
-    {
-        // An `Exception` will be thrown if this fails
-        auto config = clargs.parseConfigFile!Config;
-
-        for (size_t i = 0; config.limit == 0 || i < config.limit; ++i)
-        {
-            writeln("Hello World to you ", config.name);
-            if (config.extra_names.set)
-                writefln("And to you too, %-(%s, %)", config.extra_names.value);
-            Thread.sleep(config.frequency);
-        }
-    }
-    catch (ConfigException exc)
-    {
-        // This will print a rich error message (includes colors)
-        stderr.writefln("%S", exc);
+    // `parseConfigSimple` will print to `stderr` if an error happened
+    Nullable!Config configN = parseConfigFileSimple!Config("config.yaml");
+    if (configN.isNull())
         return 1;
-    }
-    catch (Exception exc)
+    auto config = configN.get();
+
+    // Your program goes here
+    for (size_t i = 0; config.limit == 0 || i < config.limit; ++i)
     {
-        // Other Exception type may be thrown by D-YAML,
-        // they won't include rich information.
-        stderr.writeln(exc);
-        return 1;
+        writeln("Hello World to you ", config.name);
+        if (config.extra_names.set)
+            writefln("And to you too, %-(%s, %)", config.extra_names.value);
+        Thread.sleep(config.frequency);
     }
 
     return 0;

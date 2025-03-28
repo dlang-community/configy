382767640ff95e719177d67008524ce5b6eccf9b
Mathias Lang <mlang@symmetryinvestments.com>
2025-03-28 19:20:57 +0100

    fix(node): Make `Mapping.opApply` require `@safe`

--- source/configy/backend/node.d
+++ source/configy/backend/node.d
@@ -58,13 +58,13 @@ public interface Node {
 public interface Mapping : Node {
     /// The delegate type to iterate over a mapping
     public alias MapIterator = int delegate(
-        scope Node key, scope Node value) @system;
+        scope Node key, scope Node value) @safe;
 
     /// Returns: The length of this object (the number of entries in it)
     public size_t length () const scope @safe;
 
     /// Iterates over this object, passing each entry to the `dg`
-    public int opApply (scope MapIterator dg) scope;
+    public int opApply (scope MapIterator dg) scope @safe;
 
     /// Returns: a `Node` if `key` is present in `this`, `null` otherwise
     public inout(Node) opBinaryRight(string op : "in")(string key) inout scope return @safe {
